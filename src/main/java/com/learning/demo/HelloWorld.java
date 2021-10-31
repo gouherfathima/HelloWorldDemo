faed8be66723174ef28bacf3ada1a77b7e773e26
Gouher Fathima <gouherfathima@gmail.com>
2021-10-31 18:44:41 +0000

    Initial commit

---
+++ src/main/java/com/learning/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.learning.demo;
+
+/**
+ * @Author : gouher
+ * @Description : This is demo class
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World ....!!!!");
+    }
+}
